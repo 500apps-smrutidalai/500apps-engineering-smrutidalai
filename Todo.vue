fd03224dcd117ecdb3489bdb9aa93aad41e154de
500apps-smrutidalai <88495265+500apps-smrutidalai@users.noreply.github.com>
2021-08-12 10:44:02 +0530

    Add files via upload
    https://drive.google.com/file/d/1lRpdm39knfBLVpwt3877BZkb_T2l2s9A/view

---
+++ Todo.vue
@@ -0,0 +1,71 @@
+<template>
+  <li class="d-flex align-items-center list-group-item">
+    <button
+      class="btn border-0 flex-grow-1 text-left shadow-none"
+      :class="{ completed }"
+      @click="$emit('on-toggle')"
+      v-if="!isEditing"
+    >
+      <span>{{ description }}</span>
+    </button>
+    <form v-else class="flex-grow-1" @submit.prevent="finishEditing()">
+      <input
+        type="text"
+        class="form-control"
+        v-model="newTodoDescription"
+        @blur="finishEditing()"
+        ref="newTodo"
+      />
+    </form>
+  <input type="date" v-model="date" >
+    <button
+      @click="startEditing()"
+      class="btn btn-outline-primary border-0 ml-2"
+    >
+      <span class="fa fa-edit"></span>
+    </button>
+    <button @click="$emit('on-delete')" class="btn btn-outline-danger border-0">
+      <span class="fa fa-trash"></span>
+    </button>
+  </li>
+</template>
+
+<script>
+//  import moment from 'moment'
+export default {
+  data() {
+    return {
+      isEditing: false,
+      newTodoDescription: "",
+    date: (new Date().toISOString().substr(0, 10) )
+    
+    };
+  },
+  props: {
+    description: String,
+    completed: Boolean
+  },
+  methods: {
+    startEditing() {
+      if (this.isEditing) {
+        this.finishEditing();
+      } else {
+        this.newTodoDescription = this.description;
+        this.isEditing = true;
+        this.$nextTick(() => this.$refs.newTodo.focus());
+      }
+    },
+    finishEditing() {
+      this.isEditing = false;
+      this.$emit("on-edit", this.newTodoDescription);
+    }
+    
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.completed {
+  text-decoration: line-through;
+}
+</style>
---
+++ TodoList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 py-5">
+        <h1>{{ listName }}</h1>
+      </div>
+    </div>
+    <div class="row mb-3">
+      <create-todo @on-new-todo="addTodo($event)" />
+    </div>
+    <div class="row">
+      <div class="col-12 col-sm-10 col-lg-6">
+        <ul class="list-group">
+          <todo
+            v-for="(todo, index) in todos"
+            :key="index"
+            :description="todo.description"
+            :completed="todo.completed"
+            @on-toggle="toggleTodo(todo)"
+            @on-delete="deleteTodo(todo)"
+            @on-edit="editTodo(todo, $event)"
+          />
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from "./Todo.vue";
+import CreateTodo from "./CreateTodo.vue";
+
+export default {
+  component: {
+    listName: String,
+  },
+  data() {
+    return {
+      todos: [
+        { description: "Learn HTML", completed: false },
+        { description: "Learn CSS", completed: false },
+        { description: "Learn javascript", completed: false },
+        { description: "Learn vue js", completed: false },
+      ],
+    };
+  },
+  methods: {
+    addTodo(newTodo) {
+      this.todos.push({ description: newTodo, completed: false });
+    },
+    toggleTodo(todo) {
+      todo.completed = !todo.completed;
+    },
+    deleteTodo(deletedTodo) {
+      this.todos = this.todos.filter(todo => todo !== deletedTodo);
+    },
+    editTodo(todo, newTodoDescription) {
+      todo.description = newTodoDescription;
+    },
+  },
+  components: { Todo, CreateTodo },
+};
+
+
+</script>
+
+<style scoped lang="scss"></style>var someDate = new Date('2014-05-14');
+someDate.setDate(someDate.getDate() + 15); //number  of days to add, e.x. 15 days
+var dateFormated = someDate.toISOString().substr(0,10);
+console.log(dateFormated);
